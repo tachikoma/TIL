259c9fbe6c3747833f50e517b22ba0e40569c84b
tachikoma <iam00th@hanmail.net>
2021-03-27 15:35:41 +0900

    iOS WebView Test

---
+++ ios/2021/WebViewTest/WebViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewTest
+//
+//  Created by Durk-jae Yun on 2021/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios/2021/WebViewTest/WebViewTest/WebViewController.swift
@@ -0,0 +1,56 @@
+//
+//  WebViewController.swift
+//  WebViewTest
+//
+//  Created by Durk-jae Yun on 2021/02/11.
+//
+
+import UIKit
+import WebKit
+
+final class WKCookieProcessPool: WKProcessPool {
+    static let pool = WKCookieProcessPool()
+}
+
+class CookieShareWKWebView: WKWebView {
+    override init(frame: CGRect, configuration: WKWebViewConfiguration) {
+        configuration.processPool = WKCookieProcessPool.pool  // cookie 공유
+        super.init(frame: frame, configuration: configuration)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class WebViewController: UIViewController, WKUIDelegate {
+
+    var webView: CookieShareWKWebView!
+
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = CookieShareWKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let myURL = URL(string:"https://m.daum.net")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
